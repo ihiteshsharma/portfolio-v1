c5a1210fa9f4c521aa371d0472e47073449b8cd3
Hitesh S <ihiteshsharma01@gmail.com>
2024-02-19 13:44:20 +0530

    Finalized landing page new changes

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -15,11 +15,11 @@ interface IconProps {
     onClick?: () => void
 }
 
-const Icon: FC<IconProps> = ({width=24, height=24, color="black", iconName="circle", className="", onClick=()=> {}}) => {
+const Icon: FC<IconProps> = ({width=24, height=24, color="original", iconName="circle", className="", onClick=()=> {}}) => {
     return(
         <span onClick={onClick} className={`${className}`}>
-            <svg className='fill-richblack dark:fill-paperwhite' width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" clipRule="evenodd" d={getIcon(iconName)} />
+            <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" clipRule="evenodd" d={getIcon(iconName)} fill={color} />
             </svg>
         </span>
     )
